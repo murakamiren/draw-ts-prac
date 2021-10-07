af9045db1905ae4fc3d95c308ef035a303d4bb6b
nevowo <autoeyy@gmail.com>
2021-10-07 14:45:45 +0900

    線の太さいじれるようにした

--- js/index.js
+++ js/index.js
@@ -3,33 +3,36 @@ var drawArea = document.querySelector("#draw-area");
 // console.log(drawArea.getContext);
 var drawContext = drawArea.getContext("2d");
 var deleteBtn = document.querySelector("#delete-btn");
+var widthRange = document.querySelector("#width-range");
+var setLineWidth = 1;
 var drawFlg = true;
 // console.log(drawArea.getBoundingClientRect());
 var rect = drawArea.getBoundingClientRect();
 // console.log(Math.floor(rect.top));
 var rectTop = Math.floor(rect.top);
 var rectLeft = rect.left;
-console.log(drawFlg);
+widthRange.onmouseleave = function () {
+    setLineWidth = parseInt(widthRange.value);
+};
 drawArea.onmousedown = function () {
     drawFlg = false;
-    console.log(drawFlg);
+    // console.log(drawFlg);
 };
 drawArea.onmouseup = function () {
     drawFlg = true;
-    console.log(drawFlg);
+    // console.log(drawFlg);
 };
 drawArea.onmousemove = function (e) {
     draw(e.clientX - rectLeft, e.clientY - rectTop);
 };
 // console.log(drawFlg);
 function draw(x, y) {
-    console.log(drawFlg);
     // console.log(drawFlg);
     if (drawFlg) {
         drawContext.beginPath();
         drawContext.lineCap = "round";
         drawContext.lineJoin = "round";
-        drawContext.lineWidth = 5;
+        drawContext.lineWidth = setLineWidth;
         drawContext.strokeStyle = "tomato";
         drawContext.moveTo(x, y);
     }
@@ -41,3 +44,4 @@ function draw(x, y) {
 deleteBtn.addEventListener("click", function () {
     drawContext.clearRect(0, 0, 500, 500);
 });
+console.log(setLineWidth);
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -3,6 +3,8 @@ const drawArea = document.querySelector<HTMLCanvasElement>("#draw-area");
 // console.log(drawArea.getContext);
 const drawContext = drawArea.getContext("2d");
 const deleteBtn = document.querySelector<HTMLButtonElement>("#delete-btn");
+const widthRange = document.querySelector<HTMLInputElement>("#width-range");
+let setLineWidth: number = 1;
 let drawFlg: boolean = true;
 
 // console.log(drawArea.getBoundingClientRect());
@@ -10,27 +12,30 @@ let rect: DOMRect = drawArea.getBoundingClientRect();
 // console.log(Math.floor(rect.top));
 let rectTop: number = Math.floor(rect.top);
 let rectLeft: number = rect.left;
-console.log(drawFlg);
+
+widthRange.onmouseleave = () => {
+	setLineWidth = parseInt(widthRange.value);
+};
+
 drawArea.onmousedown = () => {
 	drawFlg = false;
-	console.log(drawFlg);
+	// console.log(drawFlg);
 };
 drawArea.onmouseup = () => {
 	drawFlg = true;
-	console.log(drawFlg);
+	// console.log(drawFlg);
 };
 drawArea.onmousemove = (e) => {
 	draw(e.clientX - rectLeft, e.clientY - rectTop);
 };
 // console.log(drawFlg);
 function draw(x, y) {
-	console.log(drawFlg);
 	// console.log(drawFlg);
 	if (drawFlg) {
 		drawContext.beginPath();
 		drawContext.lineCap = "round";
 		drawContext.lineJoin = "round";
-		drawContext.lineWidth = 5;
+		drawContext.lineWidth = setLineWidth;
 		drawContext.strokeStyle = "tomato";
 		drawContext.moveTo(x, y);
 	} else {
@@ -42,3 +47,5 @@ function draw(x, y) {
 deleteBtn.addEventListener("click", () => {
 	drawContext.clearRect(0, 0, 500, 500);
 });
+
+console.log(setLineWidth);
