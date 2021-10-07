c3f4b6a2e5ad0c59bdb810e5608e3f95de1f0089
nevowo <autoeyy@gmail.com>
2021-10-07 10:06:34 +0900

    uptade

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+// console.log("hello world");
+var drawArea = document.querySelector("#draw-area");
+// console.log(drawArea.getContext);
+var drawContext = drawArea.getContext("2d");
+var drawFlg = false;
+drawArea.addEventListener("mousemove", function (e) {
+    // console.log(drawArea.getBoundingClientRect());
+    var rect = drawArea.getBoundingClientRect();
+    // console.log(rect.left);
+    // console.log(e.clientX - rect.left, e.clientY - rect.top);
+    drawArea.onmousedown = function () {
+        drawFlg = true;
+    };
+    drawArea.onmouseup = function () {
+        drawFlg = false;
+    };
+});
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,19 @@
-console.log("hello world");
+// console.log("hello world");
+const drawArea = document.querySelector<HTMLCanvasElement>("#draw-area");
+// console.log(drawArea.getContext);
+const drawContext = drawArea.getContext("2d");
+let drawFlg: boolean = false;
+
+drawArea.addEventListener("mousemove", (e) => {
+	// console.log(drawArea.getBoundingClientRect());
+	let rect: DOMRect = drawArea.getBoundingClientRect();
+	// console.log(rect.left);
+	// console.log(e.clientX - rect.left, e.clientY - rect.top);
+
+	drawArea.onmousedown = () => {
+		drawFlg = true;
+	};
+	drawArea.onmouseup = () => {
+		drawFlg = false;
+	};
+});
